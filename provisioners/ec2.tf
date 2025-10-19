04f49c282bf2efee12a103307e2859320ebc4869
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-20 00:05:32 +0800

    provisioners

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -20,6 +20,31 @@ resource "aws_instance" "ec2_instance" {
         command = "echo instance is destroyed"
         when    = destroy
     }
+    
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+   provisioner "remote-exec" {
+        inline = [
+            "sudo dnf install nginx -y",
+            "sudo systemctl start nginx "
+        ]
+
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo systemctl stop nginx",
+            "echo 'successfully stopped nginx server' "
+        ]
+
+        when = destroy
+      
+    }
 }
 
 resource "aws_security_group" "allow_all" {
