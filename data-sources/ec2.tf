1243aef5bf62851c58914c3b6f01a6ad8910fd12
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-20 00:03:20 +0800

    data-sources

---
+++ data-sources/ec2.tf
@@ -0,0 +1,37 @@
+# resource "aws_instance" "ec2_instance" {
+#     ami              = data.aws_ami.joindevops.id
+#     instance_type    = "t3.micro"
+#     vpc_security_group_ids = [aws_security_group.allow_all.id]
+#     tags = {
+#         Name = "Terraform"
+#         Terraform = "true"
+#     }
+# }
+
+# resource "aws_security_group" "allow_all" {
+#     name =            "allow_all_traffic"
+#     description =     "Security group allowing all inbound and outbound traffic"
+#     egress{
+#         from_port = 0
+#         to_port   = 0
+#         protocol  = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+
+#     }
+
+#     ingress{
+#         from_port = 0
+#         to_port   = 0
+#         protocol  = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+
+#     }
+
+#     tags = {
+#         Name = "allow_all"
+
+#     }
+# }
+
+
+
